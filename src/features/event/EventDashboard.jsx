9833139ee1ac30157828e5c4e130cd1d095589a9
Grant Best <grantbst@gmail.com>
2019-05-21 22:02:25 -0400

    test

--- src/features/event/EventDashboard.jsx
+++ src/features/event/EventDashboard.jsx
@@ -5,6 +5,7 @@ import EventList from '../event/EventList';
 import EventForm from './EventForm';
 
 const eventsDashboard = [
+    // this the eventsDashboard
     {
         id: '1',
         title: 'Trip to Tower of London',

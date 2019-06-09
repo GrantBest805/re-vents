8efa4410e24fe17ad5e6bb720e8bb8d8cfea7080
Grant Best <grantbst@gmail.com>
2019-06-09 15:37:15 -0400

    add redux form

--- src/features/event/EventListItem.jsx
+++ src/features/event/EventListItem.jsx
@@ -1,63 +1,48 @@
-import React, { Component } from "react";
-import { Segment, Item, Icon, List, Button } from "semantic-ui-react";
-
-import EventAttendees from "./EventAttendees";
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Segment, Item, Icon, List, Button } from 'semantic-ui-react';
+import EventAttendees from './EventAttendees';
+import { Link } from 'react-router-dom';
 
 class EventListItem extends Component {
-  render() {
-    const { event, deleteEvent } = this.props;
-    let hrefLink = "#";
-    return (
-      <Segment.Group>
-        <Segment>
-          <Item.Group>
-            <Item>
-              <Item.Image size='tiny' circular src={event.hostPhotoURL} />
-              <Item.Content>
-                <Item.Header as='a'>{event.title}</Item.Header>
-                <Item.Description>
-                  Hosted by
-                  <a href={hrefLink}>{` ${event.hostedBy}`}</a>
-                </Item.Description>
-              </Item.Content>
-            </Item>
-          </Item.Group>
-        </Segment>
-        <Segment>
-          <span>
-            <Icon name='clock' /> {event.date}
-            |
-            <Icon name='marker' /> {event.venue}
-          </span>
-        </Segment>
-        <Segment secondary>
-          <List horizontal>
-            {event.attendees &&
-              event.attendees.map(attendee => (
-                <EventAttendees key={attendee.id} attendee={attendee} />
-              ))}
-          </List>
-        </Segment>
-        <Segment clearing>
-          <span>{event.description}</span>
-          <Button
-            onClick={deleteEvent(event.id)}
-            as='a'
-            color='red'
-            floated='right'
-            content='Delete'
-          />
-          <Button
-            as={Link}
-            to={`/events/${event.id}`}
-            color='teal'
-            floated='right'
-            content='View'
-          />
-        </Segment>
-      </Segment.Group>
-    );
-  }
+	render() {
+		const { event, deleteEvent } = this.props;
+		let hrefLink = '#';
+		return (
+			<Segment.Group>
+				<Segment>
+					<Item.Group>
+						<Item>
+							<Item.Image size='tiny' circular src={event.hostPhotoURL} />
+							<Item.Content>
+								<Item.Header as='a'>{event.title}</Item.Header>
+								<Item.Description>
+									Hosted by
+									<a href={hrefLink}>{` ${event.hostedBy}`}</a>
+								</Item.Description>
+							</Item.Content>
+						</Item>
+					</Item.Group>
+				</Segment>
+				<Segment>
+					<span>
+						<Icon name='clock' /> {event.date}
+						|
+						<Icon name='marker' /> {event.venue}
+					</span>
+				</Segment>
+				<Segment secondary>
+					<List horizontal>
+						{event.attendees &&
+							event.attendees.map(attendee => <EventAttendees key={attendee.id} attendee={attendee} />)}
+					</List>
+				</Segment>
+				<Segment clearing>
+					<span>{event.description}</span>
+					<Button onClick={deleteEvent(event.id)} as='a' color='red' floated='right' content='Delete' />
+					<Button as={Link} to={`/events/${event.id}`} color='teal' floated='right' content='View' />
+				</Segment>
+			</Segment.Group>
+		);
+	}
 }
 export default EventListItem;

c52157038b1a87e29de01a72fa7f149234383915
Grant Best <grantbst@gmail.com>
2019-06-21 01:02:35 -0400

    add update user profile

---
+++ src/features/user/userActions.js
@@ -0,0 +1,12 @@
+import { toastr } from 'react-redux-toastr';
+
+export const updateProfile = user => async (dispatch, getState, { getFirebase }) => {
+  const firebase = getFirebase();
+  const {isLoaded, isEmpty, ...updatedUser} = user; 
+	try {
+		await firebase.updateProfile(updatedUser);
+		toastr.success('Success', 'Your profile has been updated');
+	} catch (error) {
+		console.error(error);
+	}
+};

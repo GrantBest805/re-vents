08593baba4035aecb643dd2f2ed27febff42f955
Grant Best <grantbst@gmail.com>
2019-07-13 01:22:37 -0400

    add UserDetailsHeader and UserDetailsInfo

--- src/features/user/UserDetails.jsx
+++ src/features/user/UserDetails.jsx
@@ -1,76 +1,24 @@
 import React, { Component } from 'react';
-import {
-	Button,
-	Card,
-	Grid,
-	Header,
-	Icon,
-	Image,
-	Item,
-	List,
-	Menu,
-	Segment
-} from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { Button, Card, Grid, Header, Image, Menu, Segment } from 'semantic-ui-react';
+import UserDetailsHeader from './UserDetailsHeader';
+import UserDetailsInfo from './UserDetailsInfo';
+
+const mapState = state => ({
+	auth: state.firebase.aut,
+	profile: state.firebase.profile
+});
 
 class UserDetails extends Component {
 	render() {
+		const { profile } = this.props;
 		return (
 			<Grid>
 				<Grid.Column width={16}>
-					<Segment>
-						<Item.Group>
-							<Item>
-								<Item.Image
-									avatar
-									size='small'
-									src='https://randomuser.me/api/portraits/men/20.jpg'
-								/>
-								<Item.Content verticalAlign='bottom'>
-									<Header as='h1'>First Name</Header>
-									<br />
-									<Header as='h3'>Occupation</Header>
-									<br />
-									<Header as='h3'>27, Lives in London, UK</Header>
-								</Item.Content>
-							</Item>
-						</Item.Group>
-					</Segment>
+					<UserDetailsHeader profile={profile} />
 				</Grid.Column>
 				<Grid.Column width={12}>
-					<Segment>
-						<Grid columns={2}>
-							<Grid.Column width={10}>
-								<Header icon='smile' content='About Display Name' />
-								<p>
-									I am a: <strong>Occupation Placeholder</strong>
-								</p>
-								<p>
-									Originally from <strong>United Kingdom</strong>
-								</p>
-								<p>
-									Member Since: <strong>28th March 2018</strong>
-								</p>
-								<p>Description of user</p>
-							</Grid.Column>
-							<Grid.Column width={6}>
-								<Header icon='heart outline' content='Interests' />
-								<List>
-									<Item>
-										<Icon name='heart' />
-										<Item.Content>Interest 1</Item.Content>
-									</Item>
-									<Item>
-										<Icon name='heart' />
-										<Item.Content>Interest 2</Item.Content>
-									</Item>
-									<Item>
-										<Icon name='heart' />
-										<Item.Content>Interest 3</Item.Content>
-									</Item>
-								</List>
-							</Grid.Column>
-						</Grid>
-					</Segment>
+					<UserDetailsInfo profile={profile} />
 				</Grid.Column>
 				<Grid.Column width={4}>
 					<Segment>
@@ -125,4 +73,4 @@ class UserDetails extends Component {
 	}
 }
 
-export default UserDetails;
+export default connect(mapState)(UserDetails);
---
+++ src/features/user/UserDetailsInfo.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Segment, Grid, Header, List, Item, Icon } from 'semantic-ui-react';
+import { format } from 'date-fns';
+
+const UserDetailsInfo = ({ profile }) => {
+  let occupation;
+  let origin;
+	if (profile.occupation) {
+		occupation = profile.occupation;
+	} else {
+		occupation = 'NA';
+	}
+	if (profile.origin) {
+		origin = profile.origin;
+	} else {
+		origin = 'NA';
+  }
+	return (
+		<Segment>
+			<Grid columns={2}>
+				<Grid.Column width={10}>
+					<Header icon='smile' content='About Display Name' />
+					<p>
+						I am a: <strong>{occupation}</strong>
+					</p>
+					<p>
+						Originally from: <strong>{origin}</strong>
+					</p>
+					<p>
+          Member Since: { profile.createdAt && <strong>{format(profile.createdAt.toDate(), 'LLL yyyy')}</strong> }
+					</p>
+					<p>Description of user</p>
+				</Grid.Column>
+				<Grid.Column width={6}>
+					<Header icon='heart outline' content='Interests' />
+					<List>
+						{profile.interests && profile.interests.length === 0 ? (
+							<Item>
+								<Item.Content>Really nothing interest you?</Item.Content>
+							</Item>
+						) : (
+							profile.interests && profile.interests.map(interest => (
+								<Item>
+									<Icon name='heart' />
+									<Item.Content>{interest}</Item.Content>
+								</Item>
+							))
+						)}
+					</List>
+				</Grid.Column>
+			</Grid>
+		</Segment>
+	);
+};
+
+export default UserDetailsInfo;
